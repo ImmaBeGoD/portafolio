504a19a39e386c7bcbd39912cbb5824f3651f3b2
Nicolas <rodriguezng@gmail.com>
2023-04-25 08:55:36 -0300

    Agregando los primeros archivos a Git

---
+++ script.js
@@ -0,0 +1,48 @@
+function encriptar() {
+    var texto = document.getElementById("textodesencriptado").value;
+
+    var textoencriptado = texto.replace(/e/gi,"enter");
+    var textoencriptado = textoencriptado.replace(/i/gi,"imes");
+    var textoencriptado = textoencriptado.replace(/a/gi,"ai");
+    var textoencriptado = textoencriptado.replace(/o/gi,"ober");
+    var textoencriptado = textoencriptado.replace(/u/gi,"ufat");
+
+    document.getElementById("munheco").style.display = "none";
+    document.getElementById("msj1").style.display = "none";
+    document.getElementById("msj2").style.display = "none";
+    document.getElementById("textoencriptado").style.display = "flex";
+    document.getElementById("botoncortar").style.display = "flex";
+    document.getElementById("textoencriptado").innerHTML = textoencriptado;
+}
+    
+function desencriptar() {
+    var texto = document.getElementById("textodesencriptado").value;
+
+    var textoencriptado = texto.replace(/enter/gi,"e");
+    var textoencriptado = textoencriptado.replace(/imes/gi,"i");
+    var textoencriptado = textoencriptado.replace(/ai/gi,"a");
+    var textoencriptado = textoencriptado.replace(/ober/gi,"o");
+    var textoencriptado = textoencriptado.replace(/ufat/gi,"u");
+
+    document.getElementById("munheco").style.display = "none";
+    document.getElementById("msj1").style.display = "none";
+    document.getElementById("msj2").style.display = "none";
+    document.getElementById("textoencriptado").style.display = "flex";
+    document.getElementById("botoncortar").style.display = "flex";
+    document.getElementById("textoencriptado").innerHTML = textoencriptado;
+}
+
+function cortar() {
+    var textarea = document.getElementById("textoencriptado");
+    textarea.select();
+    textarea.setSelectionRange(0, textarea.value.length);
+    document.execCommand("copy");
+
+    document.getElementById("munheco").style.display = "";
+    document.getElementById("msj1").style.display = "";
+    document.getElementById("msj2").style.display = "";
+    document.getElementById("textoencriptado").style.display = "none";
+    document.getElementById("botoncortar").style.display = "none";
+}
+
+
